fb6cadd669f757b749f590d3551fce456a82881b
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-19 12:06:52 +0200

    updated views.py

--- lims_app/views.py
+++ lims_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, "index.html", context={})
 
 def shopping(request):
     return HttpResponse("Welcome to shopping")
