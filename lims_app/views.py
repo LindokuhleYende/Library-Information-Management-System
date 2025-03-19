87384e761596a7c36fc0cf9aa6570abe2c9c6008
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-19 11:38:23 +0200

    Update views.py

--- lims_app/views.py
+++ lims_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import admin
+from django.http import HttpResponse
+
+#Register your models here
+
 
 # Create your views here.
