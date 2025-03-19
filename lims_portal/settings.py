184488bbb5729c94aae42a37ddfcd658e49b5356
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-19 13:52:11 +0200

    updated settings.py

--- lims_portal/settings.py
+++ lims_portal/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #installed apps
-    "lims_app",
+    "lims_app.apps.LimsAppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
