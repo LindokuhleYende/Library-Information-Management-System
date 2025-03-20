3277fc0b9cbbe6eb69a87124c2a32b8c0a5daff3
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-20 09:39:57 +0200

    creted a new class called books in models.py

--- lims_app/models.py
+++ lims_app/models.py
@@ -9,4 +9,13 @@ class reader(models.Model):
     reader_contact = models.CharField(max_length=200)
     reader_address = models.TextField()
     active = models.BooleanField(default=True)
-    
+
+class Book(models.Model):
+     title = models.CharField(max_length=200)
+     author = models.CharField(max_length=100)
+     isbn = models.CharField(max_length=13, unique=True)
+     publication_date = models.DateField()
+     quantity = models.PositiveIntegerField()
+     
+     def __str__(self):
+         return self.title
